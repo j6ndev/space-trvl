f30bec74ee75d5834e3325211931f00da237fd87
Jonathan Vazquez <vamtha24@gmail.com>
2018-11-12 23:56:00 -0600

    Add Datasources to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,20 @@
 const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
 
-const server = new ApolloServer({ typeDefs });
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const dataSources = () => ({
+  launchAPI: new LaunchAPI()
+});
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
